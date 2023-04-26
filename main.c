15a55bf6421c913b32fd7f6c6bb5bd90aabde683
nourrysebastieN <sebastien.nourry@epitech.eu>
2023-04-26 15:51:14 +0400

    init

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd = open("toto", O_RDWR | O_CREAT, 0644);
+
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+    printf("creating file %s....\n", "toto");
+    sleep(3);
+    dprintf(fd, "lol\n");
+    remove("toto");
+}
